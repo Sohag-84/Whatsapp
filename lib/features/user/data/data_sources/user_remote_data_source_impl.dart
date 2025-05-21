06e217ddcc1fdc1a6e7345dab57df5a928306e38
Sohag-84 <ihsohag84@gmail.com>
2025-05-21 16:33:54 +0600

    wrote user data layer code

---
+++ lib/features/user/data/data_sources/user_remote_data_source_impl.dart
@@ -0,0 +1,168 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_contacts_service/flutter_contacts_service.dart';
+import 'package:whatsapp/core/const/app_const.dart';
+import 'package:whatsapp/core/const/firebase_collection_const.dart';
+import 'package:whatsapp/features/user/data/data_sources/user_remote_data_source.dart';
+import 'package:whatsapp/features/user/data/models/user_model.dart';
+import 'package:whatsapp/features/user/domain/entities/contact_entity.dart';
+import 'package:whatsapp/features/user/domain/entities/user_entity.dart';
+
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth firebaseAuth;
+  UserRemoteDataSourceImpl({
+    required this.firestore,
+    required this.firebaseAuth,
+  });
+
+  String verificationId = "";
+
+  @override
+  Future<void> createUser(UserEntity user) async {
+    final userCollection = firestore.collection(FirebaseCollectionConst.users);
+    final uid = await getCurrentUID();
+    final newUser = UserModel(
+      username: user.username,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      isOnline: user.isOnline,
+      uid: uid,
+      status: user.status,
+      profileUrl: user.profileUrl,
+    );
+    try {
+      userCollection.doc(uid).get().then((userDoc) {
+        if (!userDoc.exists) {
+          userCollection.doc(uid).set(newUser.toDocument());
+        } else {
+          userCollection.doc(uid).update(newUser.toDocument());
+        }
+      });
+    } catch (e) {
+      throw Exception("Error occure while creating user");
+    }
+  }
+
+  @override
+  Stream<List<UserEntity>> getAllUsers() {
+    final userCollection = firestore.collection(FirebaseCollectionConst.users);
+    return userCollection.snapshots().map(
+      (querySnapshot) =>
+          querySnapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList(),
+    );
+  }
+
+  @override
+  Future<String> getCurrentUID() async {
+    return firebaseAuth.currentUser!.uid;
+  }
+
+  @override
+  Future<List<ContactEntity>> getDeviceNumber() async {
+    List<ContactEntity> contacts = [];
+    final getContactsData = await FlutterContactsService.getContacts();
+
+    for (var myContact in getContactsData) {
+      for (var phoneData in myContact.phones!) {
+        contacts.add(
+          ContactEntity(
+            phoneNumber: phoneData.value,
+            label: myContact.displayName,
+            userProfile: myContact.avatar,
+          ),
+        );
+      }
+    }
+    return contacts;
+  }
+
+  @override
+  Stream<List<UserEntity>> getSingleUser(String uid) {
+    final userCollection = firestore
+        .collection(FirebaseCollectionConst.users)
+        .where("uid", isEqualTo: uid);
+
+    return userCollection.snapshots().map(
+      (querySnapshot) =>
+          querySnapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList(),
+    );
+  }
+
+  @override
+  Future<bool> isSignIn() async => firebaseAuth.currentUser?.uid != null;
+
+  @override
+  Future<void> signInWithPhoneNumber(String smsPinCode) async {
+    try {
+      final AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId,
+        smsCode: smsPinCode,
+      );
+      await firebaseAuth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "invalid-verification-code") {
+        toast("Invalid Verfication Code");
+      }
+      if (e.code == "quota exceeded") {
+        toast("SMS Quota Exceeded");
+      }
+    } catch (e) {
+      toast(e.toString());
+    }
+  }
+
+  @override
+  Future<void> signOut() async => await firebaseAuth.signOut();
+
+  @override
+  Future<void> updateUser(UserEntity user) async {
+    final userCollection = firestore.collection(FirebaseCollectionConst.users);
+    Map<String, dynamic> userInfo = {};
+
+    if (user.username != "" && user.username != null) {
+      userInfo['username'] = user.username;
+    }
+    if (user.profileUrl != "" && user.profileUrl != null) {
+      userInfo['profileUrl'] = user.profileUrl;
+    }
+    if (user.isOnline != null) {
+      userInfo['isOnline'] = user.isOnline;
+    }
+
+    userCollection.doc(user.uid).update(userInfo);
+  }
+
+  @override
+  Future<void> verifyPhoneNumber(String phoneNumber) async {
+    phoneVerificationCompleted(AuthCredential authCredential) {
+      print(
+        "Phone verified:\nToken: ${authCredential.token}\nMethod: ${authCredential.signInMethod}",
+      );
+    }
+
+    verificationFailed(FirebaseAuthException authExeption) {
+      print(
+        "Phone verification failed:\nMessage: ${authExeption.message}\nCode: ${authExeption.code}",
+      );
+    }
+
+    codeAutoRetrievalTimeout(String verificationId) {
+      this.verificationId = verificationId;
+      print("Time out: $verificationId");
+    }
+
+    codeSent(String verificationId, int? forceResendingToken) {
+      this.verificationId = verificationId;
+    }
+
+    await firebaseAuth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: phoneVerificationCompleted,
+      verificationFailed: verificationFailed,
+      codeSent: codeSent,
+      codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
+    );
+  }
+}
---
+++ lib/features/user/data/repository/user_repository_impl.dart
@@ -0,0 +1,48 @@
+import 'package:whatsapp/features/user/data/data_sources/user_remote_data_source.dart';
+import 'package:whatsapp/features/user/domain/entities/contact_entity.dart';
+import 'package:whatsapp/features/user/domain/entities/user_entity.dart';
+import 'package:whatsapp/features/user/domain/repository/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserRemoteDataSource userRemoteDataSource;
+  const UserRepositoryImpl({required this.userRemoteDataSource});
+
+  @override
+  Future<void> createUser(UserEntity user) async =>
+      await userRemoteDataSource.createUser(user);
+
+  @override
+  Stream<List<UserEntity>> getAllUsers() => userRemoteDataSource.getAllUsers();
+
+  @override
+  Future<String> getCurrentUID() async {
+    return await userRemoteDataSource.getCurrentUID();
+  }
+
+  @override
+  Future<List<ContactEntity>> getDeviceNumber() async {
+    return await userRemoteDataSource.getDeviceNumber();
+  }
+
+  @override
+  Stream<List<UserEntity>> getSingleUser(String uid) =>
+      userRemoteDataSource.getSingleUser(uid);
+
+  @override
+  Future<bool> isSignIn() async => await userRemoteDataSource.isSignIn();
+
+  @override
+  Future<void> signInWithPhoneNumber(String smsPinCode) async =>
+      await userRemoteDataSource.signInWithPhoneNumber(smsPinCode);
+
+  @override
+  Future<void> signOut() async => await userRemoteDataSource.signOut();
+
+  @override
+  Future<void> updateUser(UserEntity user) async =>
+      await userRemoteDataSource.updateUser(user);
+
+  @override
+  Future<void> verifyPhoneNumber(String phoneNumber) async =>
+      await userRemoteDataSource.verifyPhoneNumber(phoneNumber);
+}
